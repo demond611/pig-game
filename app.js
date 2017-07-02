1f6899a5ebe1eb7ba7b88af764ae86f643f3a955
Robert <demond611@gmail.com>
2017-07-01 23:13:11 -0500

    added starting variables to app.js

--- app.js
+++ app.js
@@ -8,3 +8,40 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('current-0').textContent = 0;
+document.getElementById('score-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    // 1. random number
+    var dice = Math.floor(Math.random() * 6) + 1,
+        diceDOM = document.querySelector('.dice');
+    
+    // 2. display result
+    document.querySelector('.dice').style.display = 'block';
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    // 3. update the round score IF the rolled number was not a 1
+    
+});
+
+function NewGame() {
+    'use strict';
+    
+    document.querySelector('.dice').style.display = 'none';
+}
+
+
+
+
